09c876a0a55cf1afd7d61d55d731f1f6b30e97ac
weerza123zatereed <64904862+weerza123zatereed@users.noreply.github.com>
2020-05-09 12:30:31 +0700

    Score of some  university
    Score of computer subject

---
+++ Variable Test.py
@@ -0,0 +1,8 @@
+FounEng = 93
+GeneralBus = 97
+IntroSystem = 99
+ComPro = 100
+print("!!!your score!!!")
+print("Foundation English",":",FounEng)
+print("General Business",":",GeneralBus)
+print("Introduction to Computer Systems",":",IntroSystem)
